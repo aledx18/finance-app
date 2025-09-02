eaa7dbaaf5f8d010c8df19f1ba088f83b5b638e3
aledx18 <alediiaz18@gmail.com>
2025-09-02 12:33:41 -0300

    feat: remove authors and books API routes, update route handling for accounts

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -9,5 +9,8 @@ const app = new Hono().basePath('/api')
 const routes = app.route('/accounts', accounts)
 
 export const GET = handle(routes)
+export const POST = handle(routes)
+export const PUT = handle(routes)
+export const DELETE = handle(routes)
 
 export type AppType = typeof routes
